0bfc5bbfb83fee5c82315d48a5ee9a8c22f6edd5
Manuel Tornero <mjtf3@alu.ua.es>
2025-12-22 19:04:13 +0100

    añadimos drizzle

---
+++ database/index.ts
@@ -0,0 +1,4 @@
+import { drizzle } from 'drizzle-orm/bun-sql'
+
+// You can specify any property from the bun sql connection options
+const db = drizzle({ connection: { url: process.env.DATABASE_URL! } })
