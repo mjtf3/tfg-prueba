7512efffe2ce14b4a4102803804af933beb61a87
Manuel Tornero <manueljosetornerofuster@gmail.com>
2026-01-11 20:24:47 +0100

    componentes

--- app/pages/Login.vue
+++ app/pages/Login.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SocialSignIn from '~/components/login/SocialSignIn.vue'
 
-const { session, githubSignIn, signOut } = useAuth()
+const { githubSignIn } = useAuth()
 </script>
 
 <template>
@@ -9,7 +9,7 @@ const { session, githubSignIn, signOut } = useAuth()
     class="flex items-center justify-center min-h-screen bg-linear-to-br from-amber-50 to-orange-100"
   >
     <div class="w-full max-w-md">
-      <div v-if="!session?.data" class="space-y-6">
+      <div class="space-y-6">
         <div class="text-center space-y-2">
           <h1 class="text-3xl font-bold text-gray-900">Hola</h1>
         </div>
@@ -18,23 +18,6 @@ const { session, githubSignIn, signOut } = useAuth()
           nombre_icono="tabler:brand-github"
           @inicio_sesion="githubSignIn"
         />
-      </div>
-      <div v-else class="bg-white rounded-lg shadow-xl p-8 space-y-6">
-        <div class="text-center">
-          <h2 class="text-2xl font-bold text-gray-900 mb-2">Welcome back!</h2>
-          <p class="text-gray-600">You are signed in</p>
-        </div>
-        <div class="bg-gray-50 rounded-lg p-4 border border-gray-200">
-          <pre class="text-xs text-gray-700 overflow-auto max-h-64 font-mono">{{
-            JSON.stringify(session.data, null, 2)
-          }}</pre>
-        </div>
-        <button
-          @click="signOut()"
-          class="w-full bg-red-500 hover:bg-red-600 text-white font-semibold py-3 px-4 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-105 active:scale-95 cursor-pointer"
-        >
-          Sign out
-        </button>
       </div>
     </div>
   </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,26 @@
+<script setup lang="ts">
+const { signOut, session } = useAuth()
+</script>
+
 <template>
   <div>
     <h2>Inicio</h2>
     <p>Para hacer login haz click</p>
     <NuxtLink to="/login">AQUI</NuxtLink>
   </div>
+  <div class="text-center">
+    <h2 class="text-2xl font-bold text-gray-900 mb-2">Welcome back!</h2>
+    <p class="text-gray-600">You are signed in</p>
+  </div>
+  <div class="bg-gray-50 rounded-lg p-4 border border-gray-200">
+    <pre class="text-xs text-gray-700 overflow-auto max-h-64 font-mono">{{
+      JSON.stringify(session.data, null, 2)
+    }}</pre>
+  </div>
+  <button
+    @click="signOut()"
+    class="w-full bg-red-500 hover:bg-red-600 text-white font-semibold py-3 px-4 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-105 active:scale-95 cursor-pointer"
+  >
+    Sign out
+  </button>
 </template>
