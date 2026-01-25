3b989cf5d399dd4eb1b63f3d5daf48f34287e9d9
Manuel Tornero <manueljosetornerofuster@gmail.com>
2026-01-25 20:16:57 +0100

    asi se ve mejor la sombra del navbar

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -11,7 +11,7 @@ const closeDropdown = () => {
 
 <template>
   <!-- Esto de fondo se colorea y se pone la sombra -->
-  <div class="bg-base-100 shadow-md border-b border-gray-400">
+  <div class="bg-base-100 shadow-md border-b border-gray-400 z-10">
     <!-- Luego lo que es la navbar solo ocupa lo que ocupe container -->
     <div class="navbar container mx-auto">
       <div class="flex-1">
